42f6e564d4515212d5f667696ae3247a0f4a2502
TheMuntan <serdar_tst@hotmail.com>
2021-04-01 13:14:07 +0200

    test copied from master branch to main

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,5 @@
 public class Test {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!\nThis is a test to see if I can create a github repository through VSCode?!");
+    }
 }
