31d9e73322b8e180701862689e9c74bc6b0658ec
TheMuntan <serdar_tst@hotmail.com>
2021-04-19 10:24:26 +0200

    laptop setup

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, World!\nThis is a test to see if I can create a github repository through VSCode?!\nNow on my pc!?");
+        System.out.println("And now on my laptop as well");
     }
 }
