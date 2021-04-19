4937adcf26dd403faea53b053fd162c05fd2abd0
TheMuntan <serdar_tst@hotmail.com>
2021-04-19 08:47:03 +0200

    on pc

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, World!\nThis is a test to see if I can create a github repository through VSCode?!");
+        System.out.println("Hello, World!\nThis is a test to see if I can create a github repository through VSCode?!\nNow on my pc!?");
     }
 }
